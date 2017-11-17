e4f6f83169c4cabe4de636fb5b4c112d1de3dc16
Amanda Ackerman <ackerm44@gmail.com>
2017-11-17 18:10:06 +0000

    Outlined scraping data for Scraper class for individual vegetables

--- gardening-cli-app/gardening/lib/gardening/scraper.rb
+++ gardening-cli-app/gardening/lib/gardening/scraper.rb
@@ -6,7 +6,9 @@ module Gardening
   class Scraper
 
     def self.scrape_vegetable_list_page(list_url)
-      #list_url = File.read(list_url)
+
+
+
       doc = Nokogiri::HTML(open(list_url))
 
       vegetable_array = []
@@ -22,10 +24,25 @@ module Gardening
     end
 
     def self.scrape_vegetable_page(vegetable_url)
+      doc = Nokogiri::HTML(open(vegetable_url))
+
+      vegetable_profile_hash = {}
+
+
+      intro = doc.css("div#ContentColumn div.Liner p").first.text
+      varities = doc.css("div#ContentColumn div.Liner div.snapshot ul li[3]").first.text  #remove recommended varities and :
+      cold_tolerance = doc.css("div#ContentColumn div.Liner div.snapshot ul li[4]").first.text #same
+      sun = doc.css("div#ContentColumn div.Liner div.snapshot ul li[5]").first.text  #may not list correctly (sun ommited from some listings)
+      soil_ph = doc.css(("div#ContentColumn div.Liner div.snapshot ul li[1]")[2]).text
+      soil_temp = doc.css(("div#ContentColumn div.Liner div.snapshot ul li[2]")[2]).text
+      harvest = doc.css("div#ContentColumn div.Liner div.snapshot ul li").last.text
+      notes = doc.css("div#ContentColumn div.Liner p")[2].text
+
+
     end
 
 
   end
 end
 
-Gardening::Scraper.scrape_vegetable_list_page("http://www.growinganything.com/vegetable-planting-guide.html")
+Gardening::Scraper.scrape_vegetable_page("http://www.growinganything.com/planting-garlic.html")
