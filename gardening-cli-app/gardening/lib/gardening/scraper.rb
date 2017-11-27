5732e8a20bbfb2a8305afbdb43781679dce0d778
Amanda Ackerman <ackerm44@gmail.com>
2017-11-27 19:56:00 +0000

    automatically backed up by learn

--- gardening-cli-app/gardening/lib/gardening/scraper.rb
+++ gardening-cli-app/gardening/lib/gardening/scraper.rb
@@ -5,7 +5,7 @@ module Gardening
       doc = Nokogiri::HTML(open("http://www.growinganything.com/vegetable-planting-guide.html"))
     end
 
-    def scrape_vegetable_on_list_page
+    def self.scrape_vegetable_on_list_page
       # Due to the nature of the HTML/CSS, vegetable and url data for the list of vegetables had to be scraped
       # separately and zipped together into an array before the make_vegetables method iterates over the zipped array.
       vegetable = self.get_page.css("div#ContentColumn div.Liner ul li a[href*='growing-'] strong").map {|name| "#{name.text}"}
@@ -15,7 +15,7 @@ module Gardening
     end
 
     def self.make_vegetables
-      scrape_vegetable_on_list_page.each do |vegetable, url|
+      self.scrape_vegetable_on_list_page.each do |vegetable, url|
         Gardening::Vegetable.create_vegetables_list(vegetable, url)
       end
     end
